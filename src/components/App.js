9d898b7c7daf30ffeca7544a1efa98fd542d6493
itReverie <brenda@itreverie.com>
2018-01-02 16:29:34 -0500

    Adding Login component and constants file.

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
-import Header from './Header'
-import { Switch, Route } from 'react-router-dom'
+import Login from './Login';
+import Header from './Header';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
      <Header />
      <div className='ph3 pv1 background-gray'>
        <Switch>
+         <Route exact path='/login' component={Login}/>
          <Route exact path='/' component={LinkList}/>
          <Route exact path='/create' component={CreateLink}/>
        </Switch>
