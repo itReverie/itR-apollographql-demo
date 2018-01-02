d2883d0ce63747e7cb141d75371c6eec30f43a3d
itReverie <brenda@itreverie.com>
2018-01-02 15:14:23 -0500

    Adding my first mutation with graphql

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
-import LinkList from './LinkList'
+import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 class App extends Component {
   render() {
     return (
+      <div>
       <LinkList />
+      <CreateLink />
+     </div>
     )
   }
 }
---
+++ src/components/CreateLink.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+class CreateLink extends Component {
+
+  state = {
+    description: '',
+    url: ''
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='flex flex-column mt3'>
+          <input
+            className='mb2'
+            value={this.state.description}
+            onChange={(e) => this.setState({ description: e.target.value })}
+            type='text'
+            placeholder='A description for the link'
+          />
+          <input
+            className='mb2'
+            value={this.state.url}
+            onChange={(e) => this.setState({ url: e.target.value })}
+            type='text'
+            placeholder='The URL for the link'
+          />
+        </div>
+        <button
+          onClick={() => this._createLink()}
+        >
+          Submit
+        </button>
+      </div>
+    )
+  }
+
+// implementation of the actual mutation via the onClick on the subnit button
+_createLink = async () => {
+  const { description, url } = this.state
+
+  // note that this name 'createLinkMutation' matched with the export default
+  await this.props.createLinkMutation({
+    variables: {
+      description,
+      url
+    }
+  })
+}
+}
+
+// 1. It stores the mutation
+const CREATE_LINK_MUTATION = gql`# 2. You define the actual mutation with the two necesary arguments
+  mutation CreateLinkMutation($description: String!, $url: String!) {
+    createLink(
+      description: $description,
+      url: $url,
+    ) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
+// 3 A function called 'createLinkMutation' will be injected
+export default graphql(CREATE_LINK_MUTATION, { name: 'createLinkMutation' })(CreateLink)
