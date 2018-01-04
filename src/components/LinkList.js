a1a7b0c6b08f21d2aabe44fd8e410efcb80de4b6
itReverie <brenda@itreverie.com>
2018-01-03 19:41:10 -0500

    Adding a WebSocketLink to have a subscription for created links

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -6,17 +6,13 @@ import gql from 'graphql-tag'
 class LinkList extends Component {
 
   _updateCacheAfterVote = (store, createVote, linkId) => {
-    // 1
     const data = store.readQuery({ query: ALL_LINKS_QUERY })
 
-    // 2
     const votedLink = data.allLinks.find(link => link.id === linkId)
     votedLink.votes = createVote.link.votes
 
-    // 3
     store.writeQuery({ query: ALL_LINKS_QUERY, data })
   }
-
 
   render() {
     if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
@@ -26,7 +22,6 @@ class LinkList extends Component {
  if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
     return <div>Error</div>
   }
-
 
   const linksToRender = this.props.allLinksQuery.allLinks
 
@@ -39,6 +34,52 @@ class LinkList extends Component {
  )
 
 }//render
+
+
+_subscribeToNewLinks = () => {
+  this.props.allLinksQuery.subscribeToMore({
+    document: gql`
+      subscription {
+        Link(filter: {
+          mutation_in: [CREATED]
+        }) {
+          node {
+            id
+            url
+            description
+            createdAt
+            postedBy {
+              id
+              name
+            }
+            votes {
+              id
+              user {
+                id
+              }
+            }
+          }
+        }
+      }
+    `,
+    updateQuery: (previous, { subscriptionData }) => {
+  const newAllLinks = [
+    subscriptionData.Link.node,
+    ...previous.allLinks
+  ]
+  const result = {
+    ...previous,
+    allLinks: newAllLinks
+  }
+  return result
+}
+  })
+}
+
+componentDidMount() {
+  console.log('entering');
+  this._subscribeToNewLinks()
+}
 
 }//component
 
--- src/index.js
+++ src/index.js
@@ -10,7 +10,10 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
 import { GC_AUTH_TOKEN } from './constants'
-import { ApolloLink } from 'apollo-client-preset'
+import { ApolloLink, split } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
 
 const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjbxuyklo3siw0120rog4mlqp' })
 
@@ -29,9 +32,27 @@ const middlewareAuthLink = new ApolloLink((operation, forward) => {
 
 const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
 
+const wsLink = new WebSocketLink({
+  uri: `wss://subscriptions.graph.cool/v1/cjbxuyklo3siw0120rog4mlqp`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(GC_AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLinkWithAuthToken,
+)
 
 const client = new ApolloClient({
-  link: httpLinkWithAuthToken,
+  link,
   cache: new InMemoryCache()
 })
 
